bfe49d9d6382d00bf525c6f25360e7739df1f833
Dussart Guillaume <dussartguillaume@orange.fr>
2017-07-06 11:11:48 +0200

    main.js->add point for game

--- js/main.js
+++ js/main.js
@@ -6,30 +6,32 @@ var audio = new Audio('../song/Rocky.mp3');
 audio.play();*/
 var computerChance = 0;
 var userChance = 0;
+var userChoice;
+var userWin;
+var computerWin;
 
     var elements = ["pierre", "feuille", "ciseaux"];
     var computer = elements[Math.floor(Math.random() * elements.length)];
     var scissors = document.getElementById('scissors');
     var leaf = document.getElementById('leaf');
     var rock = document.getElementById('rock');
-    document.getElementById('user').value = userChance;
-    document.getElementById('computer').value = computerChance;
+    document.getElementById('user').value = 0;
+    document.getElementById('computer').value = 0;
     /**
      *
      * computerChoice function
      * @return result of computer choice
      *
      * */
-    var computerChoice = function (c, p, f, msgc, msgp, msgf) {
+    var computerChoice = function (msgc, msgp, msgf) {
         if (computer === 'ciseaux') {
             computer = document.getElementById('scissorsC');
             var elemC = computer;
             var posC = 0;
             var idC = setInterval(frameC, 9);
             document.getElementById('result').innerHTML = msgc;
-            c++
             function frameC() {
-                if (posC == 120) {
+                    if (posC == 120) {
                     elemC.classList.remove('translateAnimationClassC');
                     clearInterval(idC);
                 } else {
@@ -46,7 +48,6 @@ var userChance = 0;
             var posR = 0;
             var idR = setInterval(frameR, 9);
             document.getElementById('result').innerHTML = msgp;
-            p++;
 
             function frameR() {
                 if (posR == 120) {
@@ -66,7 +67,6 @@ var userChance = 0;
             var posL = 0;
             var idL = setInterval(frameL, 9);
             document.getElementById('result').innerHTML = msgf;
-            f++;
 
             function frameL() {
                 if (posL == 120) {
@@ -87,9 +87,10 @@ var userChance = 0;
      * @return user choice onclick
      *
      * */
-    var scissor = function () {
+    scissors.onclick = function () {
         leaf.classList.add('unclickable');
         rock.classList.add('unclickable');
+        userChoice = this.alt;
         var elem = scissors;
         var pos = 0;
         var id = setInterval(frame, 9);
@@ -106,8 +107,9 @@ var userChance = 0;
                 elem.style.left = pos + 'px';
             }
         }
-
-        computerChoice(null, computerChance, userChance, 'Match null', 'L\'ordi gagne', 'Vous gagne');
+        setTimeout(function(){computerChoice('Match null', 'L\'ordi gagne', 'Vous gagne');},1500)
+        
+        winner();
 
     }
     /**
@@ -116,9 +118,10 @@ var userChance = 0;
      * @return user choice onclick
      *
      * */
-    var hand = function () {
+    leaf.onclick = function () {
         scissors.classList.add('unclickable');
         rock.classList.add('unclickable');
+        userChoice = this.alt 
         var elem = leaf;
         var pos = 0;
         var id = setInterval(frame, 9);
@@ -135,8 +138,10 @@ var userChance = 0;
                 elem.style.left = pos + 'px';
             }
         }
+        setTimeout(function(){computerChoice('ordi gagne', 'vous gagne', 'match null');},1500)
 
-        computerChoice(computerChance, userChance, null, 'ordi gagne', 'vous gagne', 'match null');
+        
+        winner();
 
     }
     /**
@@ -145,14 +150,10 @@ var userChance = 0;
      * @return user choice onclick
      *
      * */
-    var stone = function () {
+    document.getElementById('rock').onclick = function () {
         leaf.classList.add('unclickable');
         scissors.classList.add('unclickable');
-
-            leaf.style.bottom = null;
-            leaf.style.left = null;
-            scissors.style.bottom = null;
-            scissors.style.left = null;
+        userChoice = this.alt;
             var elem = rock;
             var pos = 0;
             var id = setInterval(frame, 9);
@@ -169,10 +170,57 @@ var userChance = 0;
                     elem.style.left = pos + 'px';
                 }
             }
+            setTimeout(function(){computerChoice('vous gagne', 'match null', 'ordi gagne');},1500)
 
-            computerChoice(userChance, null, computerChance, 'vous gagne', 'match null', 'ordi gagne');
+
+            winner();
+        console.log(userChoice);
 
     }
+
+            console.log(computer)
+    var winner = function(){
+            if(computer === "feuille"){
+                if(userChoice === "pierre"){
+                    computerWin = true;
+                    print();
+                }else if (userChoice === "ciseaux"){
+                    userWin = true;
+                    print();
+                }
+            }else if (computer === "ciseaux"){
+                if(userChoice === "pierre"){
+                    userWin = true;
+                    print();
+                }else if(userChoice === "feuille"){
+                    computerWin = true;
+                    print();
+                }
+            }else if(computer === "pierre"){
+                if(userChoice === "feuille"){
+                    userWin = true;
+                    print();
+                }else if(userChoice === "ciseaux"){
+                    computerWin = true;
+                    print();
+                }
+            }
+
+    }
+    var print = function(){
+        if(userWin == true){
+            userChance++
+            console.log(userChance)
+    setTimeout(function(){document.getElementById('user').value = userChance;},1000)
+    
+
+        }else if (computerWin == true){
+            computerChance++
+    document.getElementById('computer').value = computerChance;
+
+        }
+    }
+
 
 
 
